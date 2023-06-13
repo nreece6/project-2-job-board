f96dba37eae71dca19bd9f34de341a69104f7fc6
Verbal Kent <abrandstrom93@gmail.com>
2023-06-12 19:11:27 -0500

    add models jobpostings and user

---
+++ models/jobPosting.js
@@ -0,0 +1,58 @@
+const { Model, DataTypes } = require('sequelize');
+
+
+const sequelize = require('../config/connection.js');
+
+
+class JobPosting extends Model {}
+
+JobPosting.init(
+    {
+        id:{
+            type:DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey:true,
+            autoIncrement:true
+          },
+          job_name:{
+            type:DataTypes.STRING,
+            allowNull:false
+
+          },
+          job_description:{
+            type:DataTypes.STRING,
+            allowNull:false
+
+          },
+          job_location:{
+            type:DataTypes.STRING,
+            allowNull:false
+
+          },
+          Company_name:{
+            type:DataTypes.STRING,
+            allowNull:false
+
+          },
+          user_id:{
+            type:DataTypes.STRING,
+            allowNull:false,
+            references:{
+                model:'user',
+                key:"id"
+              }
+
+          },
+          
+    },
+    {
+        sequelize,
+        timestamps: false,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'jobPosting',
+      }
+)
+
+
+module.exports = JobPosting
